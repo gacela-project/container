ed852b39ce092c90361ad7105e030e66d9aebac6
Jose Maria Valera Reales <jose.valera@teufel.de>
2023-04-24 13:16:22 +0200

    Allow set instance by id

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Gacela\Container;
 
-use Psr\Container\ContainerInterface;
+use Closure;
+use Gacela\Container\Exception\ContainerException;
+use SplObjectStorage;
 
+use function count;
+use function is_array;
 use function is_callable;
 use function is_object;
 
@@ -16,34 +20,148 @@ final class Container implements ContainerInterface
     /** @var array<class-string,list<mixed>> */
     private array $cachedDependencies = [];
 
+    /** @var array<string,mixed> */
+    private array $instances = [];
+
+    private SplObjectStorage $factoryInstances;
+
+    private SplObjectStorage $protectedInstances;
+
+    /** @var array<string,bool> */
+    private array $frozenInstances = [];
+
+    private ?string $currentlyExtending = null;
+
     /**
      * @param array<class-string, class-string|callable|object> $bindings
+     * @param array<string, list<Closure>> $instancesToExtend
      */
     public function __construct(
         private array $bindings = [],
+        private array $instancesToExtend = [],
     ) {
+        $this->factoryInstances = new SplObjectStorage();
+        $this->protectedInstances = new SplObjectStorage();
     }
 
     /**
      * @param class-string $className
      */
-    public static function create(string $className): ?object
+    public static function create(string $className): mixed
     {
         return (new self())->get($className);
     }
 
     public function has(string $id): bool
     {
-        return is_object($this->get($id));
+        return isset($this->instances[$id]);
+    }
+
+    public function set(string $id, mixed $instance): void
+    {
+        if (isset($this->frozenInstances[$id])) {
+            throw ContainerException::instanceFrozen($id);
+        }
+
+        $this->instances[$id] = $instance;
+
+        if ($this->currentlyExtending === $id) {
+            return;
+        }
+
+        $this->extendService($id);
     }
 
     /**
      * @param class-string|string $id
      */
-    public function get(string $id): ?object
+    public function get(string $id): mixed
     {
-        if (isset($this->bindings[$id])) {
-            $binding = $this->bindings[$id];
+        if ($this->has($id)) {
+            return $this->getInstance($id);
+        }
+
+        return $this->createInstance($id);
+    }
+
+    public function factory(Closure $instance): Closure
+    {
+        $this->factoryInstances->attach($instance);
+
+        return $instance;
+    }
+
+    public function remove(string $id): void
+    {
+        unset(
+            $this->instances[$id],
+            $this->frozenInstances[$id]
+        );
+    }
+
+    /**
+     * @psalm-suppress MixedAssignment
+     */
+    public function extend(string $id, Closure $instance): Closure
+    {
+        if (!$this->has($id)) {
+            $this->extendLater($id, $instance);
+
+            return $instance;
+        }
+
+        if (isset($this->frozenInstances[$id])) {
+            throw ContainerException::instanceFrozen($id);
+        }
+
+        if (is_object($this->instances[$id]) && isset($this->protectedInstances[$this->instances[$id]])) {
+            throw ContainerException::instanceProtected($id);
+        }
+
+        $factory = $this->instances[$id];
+        $extended = $this->generateExtendedInstance($instance, $factory);
+        $this->set($id, $extended);
+
+        return $extended;
+    }
+
+    public function protect(Closure $instance): Closure
+    {
+        $this->protectedInstances->attach($instance);
+
+        return $instance;
+    }
+
+    private function getInstance(string $id): mixed
+    {
+        $this->frozenInstances[$id] = true;
+
+        if (!is_object($this->instances[$id])
+            || isset($this->protectedInstances[$this->instances[$id]])
+            || !method_exists($this->instances[$id], '__invoke')
+        ) {
+            return $this->instances[$id];
+        }
+
+        if (isset($this->factoryInstances[$this->instances[$id]])) {
+            return $this->instances[$id]($this);
+        }
+
+        $rawService = $this->instances[$id];
+
+        /** @psalm-suppress InvalidFunctionCall */
+        $this->instances[$id] = $rawService($this);
+
+        /** @var mixed $resolvedService */
+        $resolvedService = $this->instances[$id];
+
+        return $resolvedService;
+    }
+
+    private function createInstance(string $class): ?object
+    {
+        if (isset($this->bindings[$class])) {
+            $binding = $this->bindings[$class];
             if (is_callable($binding)) {
                 /** @var mixed $binding */
                 $binding = $binding();
@@ -58,8 +176,8 @@ final class Container implements ContainerInterface
             }
         }
 
-        if (class_exists($id)) {
-            return $this->instantiateClass($id);
+        if (class_exists($class)) {
+            return $this->instantiateClass($class);
         }
 
         return null;
@@ -84,6 +202,15 @@ final class Container implements ContainerInterface
         return null;
     }
 
+    private function extendLater(string $id, Closure $instance): void
+    {
+        if (!isset($this->instancesToExtend[$id])) {
+            $this->instancesToExtend[$id] = [];
+        }
+
+        $this->instancesToExtend[$id][] = $instance;
+    }
+
     private function getDependencyResolver(): DependencyResolver
     {
         if ($this->dependencyResolver === null) {
@@ -93,5 +220,47 @@ final class Container implements ContainerInterface
         }
 
         return $this->dependencyResolver;
+    }
+
+    /**
+     * @psalm-suppress MissingClosureReturnType,MixedAssignment
+     */
+    private function generateExtendedInstance(Closure $instance, mixed $factory): Closure
+    {
+        if (is_callable($factory)) {
+            return static function (self $container) use ($instance, $factory) {
+                $r1 = $factory($container);
+                $r2 = $instance($r1, $container);
+
+                return $r2 ?? $r1;
+            };
+        }
+
+        if (is_object($factory) || is_array($factory)) {
+            return static function (self $container) use ($instance, $factory) {
+                $r = $instance($factory, $container);
+
+                return $r ?? $factory;
+            };
+        }
+
+        throw ContainerException::instanceNotExtendable();
+    }
+
+    private function extendService(string $id): void
+    {
+        if (!isset($this->instancesToExtend[$id]) || count($this->instancesToExtend[$id]) === 0) {
+            return;
+        }
+        $this->currentlyExtending = $id;
+
+        foreach ($this->instancesToExtend[$id] as $instance) {
+            $extended = $this->extend($id, $instance);
+        }
+
+        unset($this->instancesToExtend[$id]);
+        $this->currentlyExtending = null;
+
+        $this->set($id, $extended);
     }
 }
